0866193d556b17051811ecae2af2c3fd686cc156
eduardo-120 <56237939+eduardo-120@users.noreply.github.com>
2020-09-29 14:48:02 -0300

    Adicionando troca de telas

--- jogo.js
+++ jogo.js
@@ -91,14 +91,74 @@ const flappyBird = {
     }
 }
 
+// [mensagemgetReady]
+const mensagemGetReady = {
+    sX: 134,
+    sY: 0,
+    w: 174,
+    h: 152,
+    x: (canvas.width / 2) - 174 / 2,
+    y: 50,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            mensagemGetReady.sX, mensagemGetReady.sY,
+            mensagemGetReady.w, mensagemGetReady.h,
+            mensagemGetReady.x, mensagemGetReady.y,
+            mensagemGetReady.w, mensagemGetReady.h
+        );
+    }
+}
 
-function loop() {
-        flappyBird.atualiza()
+//
+// [ Telas ]
+//
+let telaAtiva = {};
+function mudaParaTela(novaTela) {
+     telaAtiva = novaTela;
+};
+const Telas = {
+    INICIO: {
+        desenha() {
+            planoDeFundo.desenha()
+            chao.desenha() 
+            flappyBird.desenha()
+            mensagemGetReady.desenha()
+        },
+        click() {
+            mudaParaTela(Telas.JOGO);
+        },
+        atualiza() {
+
+        }
+    }
+};
+
+Telas.JOGO = {
+    desenha() {
         planoDeFundo.desenha()
         chao.desenha() 
         flappyBird.desenha()
+    }, 
+    atualiza() {
+        flappyBird.atualiza()    
+    }
+};
+
+function loop() {
+
+    telaAtiva.desenha();
+    telaAtiva.atualiza();
 
     requestAnimationFrame(loop);  
 }
 
+
+window.addEventListener('click', function() {
+    if(telaAtiva.click) {
+        telaAtiva.click();
+    }
+});
+
+mudaParaTela(Telas.INICIO);
 loop();
